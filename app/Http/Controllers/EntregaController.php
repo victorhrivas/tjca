7a2c9a260976c787415afd382b05e2c806ce415f
Víctor Rivas C. <65675889+victorhrivas@users.noreply.github.com>
2026-01-28 10:46:56 -0300

    cc correos

--- app/Http/Controllers/EntregaController.php
+++ app/Http/Controllers/EntregaController.php
@@ -202,6 +202,7 @@ class EntregaController extends AppBaseController
         $to = $data['email_envio'];
 
         Mail::to($to)
+            ->cc(['jgcontador@tjca.cl','fhenott@tjca.cl'])
             ->send(new EntregaRegistradaMail($entrega->fresh(['guias']), $ot));
 
         return view('entregas.success')->with([
